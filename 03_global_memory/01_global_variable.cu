3c1e947f47606714e9c64c170cc36abb8fde24a8
Deleter-D <867909454@qq.com>
2023-10-18 21:12:56 +0800

    update

--- 03_global_memory/01_global_variable.cu
+++ 03_global_memory/01_global_variable.cu
@@ -15,12 +15,26 @@ int main(int argc, char const *argv[])
     setDevice();
 
     float value = 3.14f;
+
+    // 使用cudaMemcpyToSymbol的方式
     ERROR_CHECK(cudaMemcpyToSymbol(devData, &value, sizeof(float)));
     printf("Host:\tcopied %f to the global variable\n", value);
 
     checkGlobalVariable<<<1, 1>>>();
 
     ERROR_CHECK(cudaMemcpyFromSymbol(&value, devData, sizeof(float)));
+    printf("Host:\tthe value changed by the kernel to %f\n", value);
+
+    // 使用cudaMemcpy的方式
+    value = 3.14f;
+    float *dptr;
+    ERROR_CHECK(cudaGetSymbolAddress((void **)&dptr, devData));
+    ERROR_CHECK(cudaMemcpy(dptr, &value, sizeof(float), cudaMemcpyHostToDevice));
+    printf("Host:\tcopied %f to the global variable\n", value);
+
+    checkGlobalVariable<<<1, 1>>>();
+
+    ERROR_CHECK(cudaMemcpy(&value, dptr, sizeof(float), cudaMemcpyDeviceToHost));
     printf("Host:\tthe value changed by the kernel to %f\n", value);
 
     ERROR_CHECK(cudaDeviceReset());
