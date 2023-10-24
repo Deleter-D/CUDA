2bae28ec7876aabeeda3ac39d79a72894b5cb7ab
Deleter-D <867909454@qq.com>
2023-10-24 17:36:32 +0800

    update

--- 04_shared_and_constant_memory/01_square_shared_memory.cu
+++ 04_shared_and_constant_memory/01_square_shared_memory.cu
@@ -11,6 +11,8 @@
 #define BDIMX 32
 #define BDIMY 32
 
+#define PAD 1
+
 // 按行写入，按行读取
 __global__ void writeRowReadRow(int *out)
 {
@@ -39,6 +41,20 @@ __global__ void writeColReadCol(int *out)
     out[idx] = tile[threadIdx.x][threadIdx.y];
 }
 
+// 按列写入，按行读取
+__global__ void writeColReadRow(int *out)
+{
+    __shared__ int tile[BDIMX][BDIMY];
+
+    unsigned int idx = threadIdx.y * blockDim.x + threadIdx.x;
+
+    tile[threadIdx.x][threadIdx.y] = idx;
+
+    __syncthreads();
+
+    out[idx] = tile[threadIdx.y][threadIdx.x];
+}
+
 // 按行写入，按列读取
 __global__ void writeRowReadCol(int *out)
 {
@@ -51,6 +67,52 @@ __global__ void writeRowReadCol(int *out)
     __syncthreads();
 
     out[idx] = tile[threadIdx.x][threadIdx.y];
+}
+
+// 按行写入，按列读取，使用动态共享内存
+__global__ void writeRowReadColDynamic(int *out)
+{
+    extern __shared__ int tile[];
+
+    unsigned int row_idx = threadIdx.y * blockDim.x + threadIdx.x;
+    unsigned int col_idx = threadIdx.x * blockDim.y + threadIdx.y;
+
+    tile[row_idx] = row_idx;
+
+    __syncthreads();
+
+    out[row_idx] = tile[col_idx];
+}
+
+// 按行写入，按列读取，填充静态共享内存
+__global__ void writeRowReadColPadding(int *out)
+{
+    __shared__ int tile[BDIMY][BDIMX + PAD];
+
+    unsigned int idx = threadIdx.y * blockDim.x + threadIdx.x;
+
+    tile[threadIdx.y][threadIdx.x] = idx;
+
+    __syncthreads();
+
+    out[idx] = tile[threadIdx.x][threadIdx.y];
+}
+
+// 按行写入，按列读取，填充动态共享内存
+__global__ void writeRowReadColDynPad(int *out)
+{
+    extern __shared__ int tile[];
+
+    unsigned int row_idx = threadIdx.y * (blockDim.x + PAD) + threadIdx.x;
+    unsigned int col_idx = threadIdx.x * (blockDim.y + PAD) + threadIdx.y;
+
+    unsigned int g_idx = threadIdx.y * blockDim.x + threadIdx.x;
+
+    tile[row_idx] = g_idx;
+
+    __syncthreads();
+
+    out[g_idx] = tile[col_idx];
 }
 
 void printData(char *msg, int *in, const int size)
@@ -106,10 +168,34 @@ int main(int argc, char const *argv[])
         printData("write col read col", gpuRef, nx * ny);
 
     ERROR_CHECK(cudaMemset(d_C, 0, bytes));
+    writeColReadRow<<<grid, block>>>(d_C);
+    ERROR_CHECK(cudaMemcpy(gpuRef, d_C, bytes, cudaMemcpyDeviceToHost));
+    if (print)
+        printData("write col read row", gpuRef, nx * ny);
+
+    ERROR_CHECK(cudaMemset(d_C, 0, bytes));
     writeRowReadCol<<<grid, block>>>(d_C);
     ERROR_CHECK(cudaMemcpy(gpuRef, d_C, bytes, cudaMemcpyDeviceToHost));
     if (print)
         printData("write row read col", gpuRef, nx * ny);
+
+    ERROR_CHECK(cudaMemset(d_C, 0, bytes));
+    writeRowReadColDynamic<<<grid, block, BDIMX * BDIMY * sizeof(int)>>>(d_C);
+    ERROR_CHECK(cudaMemcpy(gpuRef, d_C, bytes, cudaMemcpyDeviceToHost));
+    if (print)
+        printData("write row read col Dyn", gpuRef, nx * ny);
+
+    ERROR_CHECK(cudaMemset(d_C, 0, bytes));
+    writeRowReadColPadding<<<grid, block>>>(d_C);
+    ERROR_CHECK(cudaMemcpy(gpuRef, d_C, bytes, cudaMemcpyDeviceToHost));
+    if (print)
+        printData("write row read col Pad", gpuRef, nx * ny);
+
+    ERROR_CHECK(cudaMemset(d_C, 0, bytes));
+    writeRowReadColDynPad<<<grid, block, BDIMX * BDIMY * sizeof(int)>>>(d_C);
+    ERROR_CHECK(cudaMemcpy(gpuRef, d_C, bytes, cudaMemcpyDeviceToHost));
+    if (print)
+        printData("write row read col DynPad", gpuRef, nx * ny);
 
     return 0;
 }
